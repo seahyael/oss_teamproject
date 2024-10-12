c92a1cde67651bf813800f7d958ece1ea6214ca0
seahyael <yael.kwon@gmail.com>
2024-10-12 18:22:20 +0900

    home.js 연결

--- src/header.js
+++ src/header.js
@@ -4,12 +4,12 @@ export default function Header() {
     return (
         <div class = "background skyblue">
             
-            <h1 class = "floatl">플레이리스트 사이트</h1>
+            <h1 class = "nav_items">플레이리스트 사이트</h1>
 
             <input type="text"/>
-            <button id = "search" class = "floatl">search</button>
+            <button id = "search" class = "nav_items">search</button>
 
-            <a href="./Home.js" class = "floatl">Home</a>
+            <a href="./Home.js" class = "nav_items">Home</a>
         </div>
     );
 }
