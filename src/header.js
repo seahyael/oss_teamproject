1a1289b2b651d840058f8c592dbb29e913038f59
seahyael <yael.kwon@gmail.com>
2024-10-12 18:25:37 +0900

    home.js 연결

--- src/header.js
+++ src/header.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./header.css";
+
 export default function Header() {
     return (
-        <div class = "background skyblue">
-            
-            <h1 class = "nav_items">플레이리스트 사이트</h1>
-
-            <input type="text"/>
-            <button id = "search" class = "nav_items">search</button>
-
-            <a href="./Home.js" class = "nav_items">Home</a>
+        <div className="background skyblue">
+            <div className="nav_items">
+                <h1>플레이리스트 사이트</h1>
+                <div>
+                    <input type="text" placeholder="검색" />
+                    <button id="search">검색</button>
+                    <a href="./Home.js">홈</a>
+                </div>
+            </div>
         </div>
     );
 }
