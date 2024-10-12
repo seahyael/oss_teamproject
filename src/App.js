79dbe58f549d7a4f0ed8de609f859ec418d41535
seahyael <yael.kwon@gmail.com>
2024-10-12 17:08:53 +0900

    header.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./header";
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
