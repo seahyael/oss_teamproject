99f9f14494fb178b0543c2dbcd56bd7034669f1e
seahyael <yael.kwon@gmail.com>
2024-10-12 17:06:00 +0900

    header.js

--- src/header.js
+++ src/header.js
@@ -1,43 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./App.css";
 export default function Header() {
-  return (
-    <div>
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-        <div className="container-fluid">
-          <Link to="/" className="navbar-brand" href="#">
-            <span className="navbar-text">React CRUD</span>
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#mynavbar"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="mynavbar">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/CRUD">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="create-user">
-                  Create User
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="show-user">
-                  Show User
-                </Link>
-              </li>
-            </ul>
-          </div>
+    return (
+        <div>
+            <h1>플레이리스트 사이트</h1>
         </div>
-      </nav>
-    </div>
-  );
+    );
 }
