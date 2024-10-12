cc2e8338f4b0a903d834a157fd6a85dbbdec4c57
seahyael <yael.kwon@gmail.com>
2024-10-12 17:19:09 +0900

    header.js

--- src/header.js
+++ src/header.js
@@ -3,11 +3,11 @@ import "./App.css";
 export default function Header() {
     return (
         <div>
-            <img src="https://img.lovepik.com/element/45015/6671.png_860.png"></img>
+            
             <h1>플레이리스트 사이트</h1>
 
             <input type="text"/>
-            <button id = "search"><img src =" https://w7.pngwing.com/pngs/242/281/png-transparent-computer-icons-magnifier-symbol-magnifying-glass-magnifying-miscellaneous-logo-zooming-user-interface.png"></img></button>
+            <button id = "search">search</button>
 
             <h2>Home</h2>
         </div>
