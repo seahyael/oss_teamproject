79902fb0d0c6b8fc5f106bc2466e2f6d67f1e880
seahyael <yael.kwon@gmail.com>
2024-10-12 17:31:53 +0900

    header.css

--- src/header.js
+++ src/header.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./header.css";
 export default function Header() {
     return (
-        <div>
+        <div class = "background skyblue">
             
-            <h1 class = "float">플레이리스트 사이트</h1>
+            <h1 class = "floatl">플레이리스트 사이트</h1>
 
             <input type="text"/>
-            <button id = "search" class="float">search</button>
+            <button id = "search">search</button>
 
-            <h2>Home</h2>
+            <h2 class = "floatr">Home</h2>
         </div>
     );
 }
