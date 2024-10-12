79da55dba5e63d403470293ad829cd126a2608f5
seahyael <yael.kwon@gmail.com>
2024-10-12 17:29:13 +0900

    header.css

--- src/header.js
+++ src/header.js
@@ -4,12 +4,12 @@ export default function Header() {
     return (
         <div>
             
-            <h1>플레이리스트 사이트</h1>
+            <h1 class = "float">플레이리스트 사이트</h1>
 
             <input type="text"/>
             <button id = "search" class="float">search</button>
 
-            <h2 class="float">Home</h2>
+            <h2>Home</h2>
         </div>
     );
 }
