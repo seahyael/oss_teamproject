fea3c2450ff3f319f0a05aa63cca145388461701
seahyael <yael.kwon@gmail.com>
2024-10-20 21:59:36 +0900

    home.js 수정

--- src/components/contents/home/Home.js
+++ src/components/contents/home/Home.js
@@ -1,5 +1,5 @@
 import "./Home.css";
-
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 
 function Home() {
@@ -7,47 +7,48 @@ function Home() {
     <div className="home-container">
       <div className="home-title">
         <h1>My Playlists</h1>
-        <Link to="/create">create playlist</Link>
+        <Link to="/create">Create Playlist</Link>
       </div>
       <div className="contents">
-        <div className="item">
-          <PlaylistCard />
-        </div>
-        <div className="item">
-          <PlaylistCard />
-        </div>
-        <div className="item">
-          <PlaylistCard />
-        </div>
-        <div className="item">
-          <PlaylistCard />
-        </div>
-        <div className="item">
-          <PlaylistCard />
-        </div>
+        <PlaylistCard />
       </div>
     </div>
   );
 }
 
-function PlaylistCard(props) {
+function PlaylistCard() {
+  const [playlists, setPlaylists] = useState([]);
+
+  const fetchPlaylists = async () => {
+    try {
+      const response = await fetch('https://6713c2f7690bf212c75faacf.mockapi.io/playlist');
+      if (!response.ok) {
+        throw new Error('네트워크 응답이 좋지 않습니다.');
+      }
+      const data = await response.json();
+      setPlaylists(data);
+    } catch (error) {
+      console.error('데이터를 가져오는 중 오류 발생:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPlaylists();
+  }, []);
+
   return (
-    <div className="card" style={{ width: "15rem" }}>
-      <img
-        src="https://media.istockphoto.com/id/1470710940/ko/%EC%82%AC%EC%A7%84/%ED%9D%B0%EC%83%89-%EB%B0%94%ED%83%95%EC%97%90-%EC%95%84%EB%A6%84-%EB%8B%A4%EC%9A%B4-%EC%96%BC%EB%A3%A9-%EA%B3%A0%EC%96%91%EC%9D%B4.jpg?s=2048x2048&w=is&k=20&c=k3JOZES3LF1zP5K-krGgJ0hw4fduWhhl4DV6-4ddjCg="
-        class="card-img-top"
-        alt="..."
-      />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="http://localhost:3000/home" class="btn btn-primary">
-          Go somewhere
-        </a>
-      </div>
+    <div className="card-container d-flex flex-wrap justify-content-center">
+      {playlists.map((playlist) => (
+        <div className="card" style={{ width: "15rem", margin: "10px" }} key={playlist.id}>
+          <div style={{ backgroundColor: playlist.color, height: "150px" }}>
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">{playlist.title}</h5>
+            <p className="card-text">{playlist.description}</p>
+            <a href={playlist.url} className="btn btn-primary">View Playlist</a>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
